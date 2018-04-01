02c8263ba8f92aaf0ca9f619ec77997936a01c02
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2018-04-01 20:56:10 +0200

    Open child memory only once

--- emulator.c
+++ emulator.c
@@ -21,120 +21,149 @@
 
 #define IS_ERR(x) ((x) == -1)
 #define IS_PTRACE_ERR(x) ((x) == -1 && errno)
-// Execute successfully or exit immediately
-#define MUST(x) if (IS_ERR(x)) return -1;
 
-static void emulation_failure(const char *message) {
+/// AlienOS process information for the tracer.
+struct alien_proc_struct {
+    pid_t pid;
+    int mem_fd;
+};
+
+/// Prints an error and exits.
+/// \param message error message.
+static void system_failure(const char *message) {
     end_alienos();
     fprintf(stderr, "%s\n", message);
     exit(EXIT_ALIENOS_FAIL);
 }
 
-static void emulate_end(__attribute__((unused)) pid_t pid, struct user_regs_struct *regs) {
+/// Prints an error and exits cleaning up data opened by the tracer.
+/// \param child alien process tracer data.
+/// \param message error message.
+static void emulation_failure(struct alien_proc_struct *child, const char *message) {
+    close(child->mem_fd);
+    system_failure(message);
+}
+
+/// Emulates sys_end syscall.
+/// \param child alien process tracer data.
+/// \param regs alien process registers data.
+static void emulate_end(struct alien_proc_struct *child, struct user_regs_struct *regs) {
+    close(child->mem_fd);
     sys_end((int) regs->rdi);
 }
 
-static void emulate_getrand(pid_t pid, struct user_regs_struct *regs) {
+/// Emulates sys_getrand syscall.
+/// \param child alien process tracer data.
+/// \param regs alien process registers data.
+static void emulate_getrand(struct alien_proc_struct *child, struct user_regs_struct *regs) {
     uint32_t value;
     if (IS_ERR(sys_getrand(&value))) {
-        emulation_failure("sys_getrand");
+        emulation_failure(child, "sys_getrand");
     }
     regs->rax = value;
 
-    if (IS_PTRACE_ERR(ptrace(PTRACE_SETREGS, pid, NULL, regs))) {
-        emulation_failure("PTRACE_SETREGS");
+    if (IS_PTRACE_ERR(ptrace(PTRACE_SETREGS, child->pid, NULL, regs))) {
+        emulation_failure(child, "PTRACE_SETREGS");
     }
 }
 
-static void emulate_getkey(pid_t pid, struct user_regs_struct *regs) {
+/// Emulates sys_getkey syscall.
+/// \param child alien process tracer data.
+/// \param regs alien process registers data.
+static void emulate_getkey(struct alien_proc_struct *child, struct user_regs_struct *regs) {
     regs->rax = (long long unsigned int) sys_getkey();
-    if (IS_PTRACE_ERR(ptrace(PTRACE_SETREGS, pid, NULL, regs))) {
-        emulation_failure("PTRACE_SETREGS");
+    if (IS_PTRACE_ERR(ptrace(PTRACE_SETREGS, child->pid, NULL, regs))) {
+        emulation_failure(child, "PTRACE_SETREGS");
     }
 }
 
-static int read_memory(pid_t pid, void *buffer, size_t buffer_len, off_t address) {
-    int fd;
-    char file[64];
-
-    MUST(sprintf(file, "/proc/%ld/mem", (long) pid))
-    MUST(fd = open(file, O_RDWR))
-
-    if (pread(fd, buffer, buffer_len, address) != (ssize_t) buffer_len) {
-        (void) close(fd);
-        return -1;
-    }
-
-    MUST(close(fd))
-    return 0;
-}
-
-static void emulate_print(pid_t pid, struct user_regs_struct *regs) {
+/// Emulates sys_print syscall.
+/// \param child alien process tracer data.
+/// \param regs alien process registers data.
+static void emulate_print(struct alien_proc_struct *child, struct user_regs_struct *regs) {
     size_t buffer_len = sizeof(uint16_t) * regs->r10;
     uint16_t *buffer = (uint16_t *) malloc(buffer_len);
     if (buffer == NULL) {
-        emulation_failure("malloc");
+        emulation_failure(child, "malloc");
     }
 
-    if (read_memory(pid, buffer, buffer_len, (off_t) regs->rdx) == -1) {
+    if (pread(child->mem_fd, buffer, buffer_len, (off_t) regs->rdx) != (ssize_t) buffer_len) {
         free(buffer);
-        emulation_failure("read_memory");
+        emulation_failure(child, "pread");
     }
 
     if (IS_ERR(sys_print((int) regs->rdi, (int) regs->rsi, buffer, (int) regs->r10))) {
         free(buffer);
-        emulation_failure("sys_print");
+        emulation_failure(child, "sys_print");
     }
     free(buffer);
 }
 
-static void emulate_setcursor(__attribute__((unused)) pid_t pid, struct user_regs_struct *regs) {
+/// Emulates sys_setcursor syscall.
+/// \param child alien process tracer data.
+/// \param regs alien process registers data.
+static void emulate_setcursor(struct alien_proc_struct *child, struct user_regs_struct *regs) {
     if (IS_ERR(sys_setcursor((int) regs->rdi, (int) regs->rsi))) {
-        emulation_failure("sys_setcursor");
+        emulation_failure(child, "sys_setcursor");
     }
 }
 
-static void emulate_syscall(pid_t pid) {
+/// Emulates alien process syscall.
+/// \param child alien process tracer data.
+static void emulate_syscall(struct alien_proc_struct *child) {
     struct user_regs_struct regs;
 
-    long err = ptrace(PTRACE_GETREGS, pid, NULL, &regs);
+    long err = ptrace(PTRACE_GETREGS, child->pid, NULL, &regs);
     if (IS_PTRACE_ERR(err)) {
-        emulation_failure("PTRACE_GETREGS");
+        emulation_failure(child, "PTRACE_GETREGS");
     }
 
     switch (regs.orig_rax) {
         case SYSCALL_END:
-            emulate_end(pid, &regs);
+            emulate_end(child, &regs);
             break;
         case SYSCALL_GETRAND:
-            emulate_getrand(pid, &regs);
+            emulate_getrand(child, &regs);
             break;
         case SYSCALL_GETKEY:
-            emulate_getkey(pid, &regs);
+            emulate_getkey(child, &regs);
             break;
         case SYSCALL_PRINT:
-            emulate_print(pid, &regs);
+            emulate_print(child, &regs);
             break;
         case SYSCALL_SETCURSOR:
-            emulate_setcursor(pid, &regs);
+            emulate_setcursor(child, &regs);
             break;
         default:
-            end_alienos();
-            fprintf(stderr, "Invalid syscall %llu\n", regs.orig_rax);
-            exit(EXIT_ALIENOS_FAIL);
+            emulation_failure(child, "unknown syscall");
     }
+}
+
+static int map_child_mem(pid_t pid) {
+    char file[64];
+    if (sprintf(file, "/proc/%ld/mem", (long) pid) == -1) {
+        return -1;
+    }
+    return open(file, O_RDWR);
 }
 
 void run_emulator(pid_t pid, int argc, char *argv[]) {
     int status;
+    struct alien_proc_struct child = { .pid = pid, .mem_fd = -1 };
+
     if (start_alienos() != 0) {
-        emulation_failure("Unable to start AlienOS emulator");
+        system_failure("Unable to start AlienOS emulator");
     }
 
     // Allow the tracee to execve.
     (void) wait(&status);
     if (IS_PTRACE_ERR(ptrace(PTRACE_SYSCALL, pid, NULL, NULL))) {
-        emulation_failure("PTRACE_SYSCALL");
+        system_failure("PTRACE_SYSCALL");
+    }
+
+    // Open tracee memory
+    if (IS_ERR(child.mem_fd = map_child_mem(pid))) {
+        system_failure("map_child_mem");
     }
 
     do {
@@ -142,9 +171,9 @@ void run_emulator(pid_t pid, int argc, char *argv[]) {
         if (pid == -1 || WIFEXITED(status)) {
             sys_end(status);
         } else if (WSTOPSIG(status) == SIGTRAP) {
-            emulate_syscall(pid);
+            emulate_syscall(&child);
             if (IS_PTRACE_ERR(ptrace(PTRACE_SYSEMU, pid, NULL, NULL))) {
-                emulation_failure("PTRACE_SYSEMU");
+                emulation_failure(&child, "PTRACE_SYSEMU");
             }
         }
     } while (1);
